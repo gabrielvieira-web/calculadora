f95af667ab3cdd0ead5623b2a7b92c8496953b16
gabrielvieira-web <vieira.github@gmail.com>
2023-07-12 11:05:13 -0300

    Calculadora Finalizada

--- js/script.js
+++ js/script.js
@@ -8,18 +8,30 @@ const resultado = document.querySelector("[data-resultado]");
 numeros.forEach((numero) => numero.addEventListener("click", (e) => {
 
     const numero = e.target.innerText;
+
+    if (resultado.innerHTML.includes(".") && numero === "." || resultado.innerHTML === "" && numero === ".") return;
+
     resultado.innerHTML = resultado.innerHTML + numero;
 
 }));
 
 operadores.forEach((operador) => operador.addEventListener("click", (e) => {
 
-    const operador = e.target.innerText
-    if (resultado.innerHTML === "") {
-        return;
-    } else {
-        resultado.innerHTML = resultado.innerHTML + operador;
+    const operador = e.target.innerText;
+    const ultimoElementoDoResultado = resultado.innerHTML.length -1;
+    const operadorAnterior = resultado.innerHTML.slice(ultimoElementoDoResultado);
+
+    
+    
+    if (resultado.innerHTML === "" || resultado.innerHTML.lastIndexOf(operador) === ultimoElementoDoResultado) return;
+        
+    if (operadorAnterior === "+" || operadorAnterior === "-" || operadorAnterior === "x" || operadorAnterior === "/") {
+        if (operador != operadorAnterior) {
+            return;
+        }
     };
+    
+    resultado.innerHTML = resultado.innerHTML + operador;
 
 }));
 
@@ -37,7 +49,7 @@ botaoigualdade.addEventListener("click", () => {
         return;
     } else {
         if (resultado.innerHTML.indexOf("x") > 0) {
-            const trocaFormatoDoOperador = resultado.innerHTML.replace("x", "*")
+            const trocaFormatoDoOperador = resultado.innerHTML.replace(/x/g, "*");
             resultado.innerHTML = eval(trocaFormatoDoOperador);                                                                                                                                                                                                                                                       
         } else {
             resultado.innerHTML = eval(resultado.innerHTML);
